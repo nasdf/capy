61a208acf17b1a4178d03343cc54e6659644ba2c
nasdf <keenan.nemetz@gmail.com>
2024-12-07 13:43:34 -0800

    simplify export

--- core/export.go
+++ core/export.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"io"
 
-	"github.com/ipfs/go-cid"
 	"github.com/ipld/go-car/v2"
 	"github.com/ipld/go-ipld-prime/datamodel"
+	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
 	"github.com/ipld/go-ipld-prime/node/basicnode"
 	"github.com/ipld/go-ipld-prime/traversal/selector"
 	"github.com/ipld/go-ipld-prime/traversal/selector/builder"
@@ -14,15 +14,11 @@ import (
 
 // Export writes a CAR containing the DAG starting from the given root link to the given io.Writer.
 func Export(ctx context.Context, store *Store, rootLink datamodel.Link, out io.Writer) error {
-	root, err := cid.Decode(rootLink.String())
-	if err != nil {
-		return err
-	}
-
+	cid := rootLink.(cidlink.Link).Cid
 	ssb := builder.NewSelectorSpecBuilder(basicnode.Prototype.Any)
 	sel := ssb.ExploreRecursive(selector.RecursionLimitNone(), ssb.ExploreAll(ssb.ExploreRecursiveEdge()))
 
-	w, err := car.NewSelectiveWriter(ctx, &store.lsys, root, sel.Node())
+	w, err := car.NewSelectiveWriter(ctx, &store.lsys, cid, sel.Node())
 	if err != nil {
 		return err
 	}
